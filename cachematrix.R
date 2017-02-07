c19e23365d9e0f15eb06c878359e3127eb46bcf6
owusuessel <owusuessel@gmail.com>
2017-02-07 00:11:23 +0000

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+-## Put comments here that give an overall description of what your
+ -## functions do
+ -
+ -## Write a short comment describing this function
+ -
+ -makeCacheMatrix <- function(x = matrix()) {
+ -
+ -}
+ -
+ -
+ -## Write a short comment describing this function
+ -
+ -cacheSolve <- function(x, ...) {
+ -        ## Return a matrix that is the inverse of 'x'
+ -}
+ +## Put comments here that give an overall description of what your
+ +## functions do
+ +
+ +## This function creates a special Matrix object that can
+ +## cache its inverse
+ +
+ +makeCacheMatrix <- function(x = matrix()) {
+ +  m <- NULL
+ +  set <- function(y) {
+ +    x <<- y
+ +    m <<- NULL
+ +  }
+ +  get <- function() x
+ +  setsolve <- function(solve) m <<- solve
+ +  getsolve <- function() m
+ +  list(set = set, get = get,
+ +       setsolve = setsolve,
+ +       getsolve = getsolve)
+ +}
+ +
+ +
+ +## This function computes the inverse of the special "matrix" 
+ +## returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), 
+ +## then the cachesolve retrieves the inverse from the cache.
+ +cacheSolve <- function(x, ...) {
+ +        
+ +  m <- x$getsolve()
+ +  if(!is.null(m)) {
+ +    message("getting cached data")
+ +    return(m)
+ +  }
+ +  data <- x$get()
+ +  m <- solve(data, ...)
+ +  x$setsolve(m)
+ +  m
+ +}
+ +
+ +##Define a square Matrix
+ +A = matrix(c(2, 4, 3, 1),nrow=2,ncol=2,byrow=TRUE)
+ +##To run the matrix through the code define an object,eg Mat <-makeCacheMatrix(A)
+ +##Then cacheSolve(Mat)
+ +## If you repeat the last line of code you will get the "getting cached data" message
